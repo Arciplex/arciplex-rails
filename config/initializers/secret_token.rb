3d470f09c8a8bd9e06c66b377ac9506954420047
Dennis Monsewicz <dennismonsewicz@gmail.com>
2014-01-29 11:26:01 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arciplex::Application.config.secret_key_base = 'f70e30bc68523eda776c923d7db1f4da6fb8d61fb124d31d9915cdbeed68ba711c11b1722eff6b07663762b8a9cbf416378c446d441ee9d6a53264c9b6f22ed4'
