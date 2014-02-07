9eae5dfc9e5e7ba1fe0ad85a4735254aee81a558
Dennis Monsewicz <dennismonsewicz@gmail.com>
2014-02-06 22:50:28 -0600

    adding in some user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,26 @@ require 'spec_helper'
 
 describe User do
   let(:company) { create(:company) }
+  let(:user) { build(:user) }
   
   before do
     company
+    user.company_id = company.id
   end
   
   describe "#create" do
     context "when normal user is created" do
       it "should set .role to requestor" do
-        user = create(:user)
-        user.company_id = company.id
         user.save
-        user.role.should eq "requestor"
+        user.role.should eq User::ROLES.last
+      end
+    end
+    
+    context "when admin user is created" do
+      it "should set .role to admin" do
+        user.role = User::ROLES[1]
+        user.save
+        user.role.should eq User::ROLES[1]
       end
     end
   end
