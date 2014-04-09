8b8683641928188b321092412bad2fb8f3f7018a
Dennis Monsewicz <dmonsewicz@myemma.com>
2014-04-09 08:16:40 -0500

    fixing issue where all service requests where showing up regardless of the company you were looking at

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_ability
-    @current_ability ||= Ability.new(current_user, params[:company_id])
+    @current_ability ||= Ability.new(current_user)
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,11 @@
 class Ability
   include CanCan::Ability
   
-  def initialize(user, company_id)
+  def initialize(user)
     @user = user
     
     if @user.admin?
       can :manage, :all
-      can :manage, ServiceRequest, company_id: company_id unless company_id.nil?
     else
       can [:create, :read], ServiceRequest, company_id: @user.company_id
       can :manage, Customer, company_id: @user.company_id
