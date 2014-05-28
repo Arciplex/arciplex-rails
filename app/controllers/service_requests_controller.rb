7dfe444f7f44f04dc01cfe5e5ece461185a4223b
Dennis Monsewicz <dmonsewicz@myemma.com>
2014-05-28 14:16:34 -0500

    updating SR views to show SR ID vs randomly generated case number

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -2,32 +2,32 @@ class ServiceRequestsController < ApplicationController
   before_filter :authenticate_user!, :get_company
   authorize_resource except: [:index, :update]
   load_resource except: [:create, :update]
-  
+
   respond_to :html, :js
-  
+
   def index
     @service_requests = @company.service_requests.where('status != ?', 'closed') unless params[:status]
     @service_requests = @service_requests.send(params[:status]) if params[:status]
     @service_requests = @service_requests.paginate(page: params[:page])
   end
-  
+
   def show
     respond_with @service_request
   end
-  
+
   def new
     @service_request.line_items.build
     @service_request.build_customer
   end
-  
+
   def edit
     respond_with @service_request
   end
-  
+
   def create
     @service_request = current_user.service_requests.new(service_request_params)
     @service_request.company_id = current_user.admin? ? params[:company_id] : current_user.company_id
-    
+
     if @service_request.save
       @service_request.notify
       redirect_to company_service_request_path(company_id: params[:company_id], id: @service_request), notice: "Serice Request successfully created!"
@@ -35,14 +35,14 @@ class ServiceRequestsController < ApplicationController
       render :new
     end
   end
-  
+
   def update
     @service_request = ServiceRequest.find(params[:id])
-    
+
     if @service_request.update_attributes(service_request_params)
       @service_request.notify
       respond_to do |format|
-        format.html { redirect_to company_service_request_path(company_id: params[:company_id], id: @service_request), notice: "#{@service_request.case_number} updated successfully!" }
+        format.html { redirect_to company_service_request_path(company_id: params[:company_id], id: @service_request), notice: "#{@service_request.id} updated successfully!" }
         format.js
       end
     else
@@ -52,7 +52,7 @@ class ServiceRequestsController < ApplicationController
       end
     end
   end
-  
+
   def destroy
     @service_request = ServiceRequest.find(params[:id])
     if @service_request.complete!
@@ -61,12 +61,12 @@ class ServiceRequestsController < ApplicationController
       redirect_to company_service_requests_path(company_id: params[:company_id]), notice: "An error occurred. Please try again!"
     end
   end
-  
+
   def received
     @service_request = ServiceRequest.find(params[:id])
-    
+
     authorize! :manage, @service_request
-    
+
     respond_to do |format|
       if @service_request.received!
         @service_request.notify
@@ -76,7 +76,7 @@ class ServiceRequestsController < ApplicationController
       end
     end
   end
-  
+
   private
     def service_request_params
       service_request_attributes = [
